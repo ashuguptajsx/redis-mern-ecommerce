d29258faa661cf18e0a6df593acf0b193fce21cb
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2025-01-18 00:14:17 +0530

    commit

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -125,7 +125,7 @@ export const logout = async (req, res) => {
 };
 
 
-export const refreshTokens = async (req, res) =>{
+export const refreshToken = async (req, res) =>{
   try {
     const refreshToken = req.cookies.refreshToken;
 
@@ -134,7 +134,26 @@ export const refreshTokens = async (req, res) =>{
         message:"NO refresh tokes found"
       })
     }
+    const decoded = jwt.verify(refreshToken, process.env.REFRESH_JWT_SECRET);
+    const storedToken = await redis.get(`refreshToken:${decoded.userId}`);
+
+    if(storedToken !==refreshToken){
+      return res.status(401).json({message:"Invalid refresh token"})
+    }
+
+    const accessToken = jwt.sign({userId:decoded.userId}, process.env.JWT_SECRET, { expiresIn:"15m"});
+
+    res.cookie("accessToken", accessToken, {
+      httpOnly:true,
+      secure: process.env.NODE_ENV === "production",
+      sameSite:"strict",
+      maxAge: 15*60*1000
+    });
+
+    res.json({message:"token refreshed successfully"})
   } catch (error) {
+    console.log("error in refreshing the token", error.message)
+    res.status(500).json({message:"server error", error:error.message});
     
   }
 }
