878ab1f3bc130f5b1c5fe28ce5e15a29f4e9a020
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2025-02-08 23:41:13 +0530

    commit

--- backend/controllers/analytics.controller.js
+++ backend/controllers/analytics.controller.js
@@ -1,3 +1,64 @@
-export const  getAnalytics = async() =>{
+import User from "../models/user.model.js"
+import Product from "../models/product.model.js"
+import Order from "../models/order.model.js"
+
+export const  getAnalyticsData = async() =>{
+    const totalUsers = await User.countDocuments();
+    const totalProducts = await Product.countDocuments();
+
+    const salesData = await Order.aggregate([
+        {
+            $group:{
+                _id: null,
+                totalSales: {$sum: 1},
+                totalRevenue : {$sum:"$totalAmount"} 
+            }
+        }
+    ])
+
+    const {totalSales, totalRevenue} = salesData[0] || {totalSales: 0, totalRevenue: 0} ; 
+
+
+    return {
+        users: totalUsers,
+        products: totalProducts,
+        totalSales,
+         totalRevenue
+
+    }
+}
+
+export const getDailySalesData = async(startDate, endDate) =>{
+    return Order.aggregate([
+        {
+            $match:{
+                createdAt: {
+                    $gte: startDate,
+                    $lte: endDate
+                }
+            }
+        },
+        {
+            $group:{
+                _id: {$dateToString: {format: "%Y-%m-%d", date: "$createdAt"}},
+                sales: {$sum: 1},
+                Revenue: {$sum: "$totalAmount"}
+            }
+        },
+        { $sort: { _id: 1 } },
+    ]);
+    // [
+    //     {
+    //         _id : "2025-08-02",
+    //         sales: 5,
+    //         Revenue: 500
+    //     },
+    // ]
+
+
+    const dateArray = 
+}
+
+function getDateInRange(startDate, endDate){
     
 }
--- backend/routes/analytics.routes.js
+++ backend/routes/analytics.routes.js
@@ -1,13 +1,25 @@
 import express from "express";
-import { adminRoute, protectRoute } from "../middleware/auth.middleware";
+import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
+import { getAnalyticsData } from "../controllers/analytics.controller.js";
 
 const router =  express.Router();
 
 router.get("/", protectRoute, adminRoute, async(req, res) =>{
     try {
-        const analytics = await getAnalytics();
-    } catch (error) {
+        const analyticsData = await getAnalyticsData();
+
         
+        const endDate  = Date.now();
+        const startDate = new Date(endDate - 7 * 24 * 60 * 60 * 1000); 
+
+        const dailySalesData = await getDailySalesData(startDate, endDate);
+        res.json({
+            analyticsData,
+            dailySalesData 
+        })
+    } catch (error) {
+        console.log("Error getting analytics route", error.message);
+        res.status(500).json({message: "Server eror", error:error.message});
     }
 })
 
