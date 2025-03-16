140e39ba37b2e8c5243d17141694b769de9a2291
ashu-1403 <129203568+ashu-1403@users.noreply.github.com>
2025-03-16 23:48:02 +0530

    commit

--- frontend/src/components/ProductCard.jsx
+++ frontend/src/components/ProductCard.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import toast from 'react-hot-toast'
+import {ShoppingCart} from "lucide-react"
 
 const ProductCard = () => {
   return (
-    <div>ProductCard</div>
+    <div className='flex w-full relative flex-col overflow-hidden border-gray-700 shadow-lg rounded-lg'>
+      <div className='relative mx-3 mt-3 flex h-60 overflow-hidden rounded-xl'>
+				<img className='object-cover w-full' src={product.image} alt='product image' />
+				<div className='absolute inset-0 bg-black bg-opacity-20' />
+			</div>
+
+    </div>
   )
 }
 
